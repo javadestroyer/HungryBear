260170450587d65f4063fad63556393b35b7d8da
Bassel Rafie <Bassel.Rafie@gmail.com>
2018-12-25 16:00:34 +0100

    Initial commit

---
+++ app/src/main/java/basselrafie/hungrybear/Splash.java
@@ -0,0 +1,44 @@
+package basselrafie.hungrybear;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Splash extends AppCompatActivity {
+    private TextView tv;
+    private ImageView iv;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        tv = (TextView) findViewById(R.id.tv);
+        iv = (ImageView) findViewById(R.id.iv);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.mytransition);
+        tv.startAnimation(myanim);
+        iv.startAnimation(myanim);
+        final Intent i = new Intent(this,MainActivity.class);
+
+        Thread timer = new Thread(){
+
+            @Override
+            public void run() {
+                try {
+                    sleep(3500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(i);
+
+                }
+            }
+        };
+        timer.start();
+    }
+}
