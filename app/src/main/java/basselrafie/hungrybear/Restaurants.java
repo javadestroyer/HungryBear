0dd163155e45006f4a58078ef43bffecf44fd5cd
VouttaRene <r.voutta@web.de>
2019-01-16 16:44:54 +0100

    Kategorien in der Database angelegt

--- app/src/main/java/basselrafie/hungrybear/Restaurants.java
+++ app/src/main/java/basselrafie/hungrybear/Restaurants.java
@@ -15,8 +15,32 @@ public class Restaurants {
     @ColumnInfo(name = "RestaurantName")
     private String rname;
 
-    //@ColumnInfo(name = "Salat")
-    //private String rsalat;
+    @ColumnInfo(name = "Döner")
+    private String rdoener;
+
+    @ColumnInfo(name = "Pizza")
+    private String rpizza;
+
+    @ColumnInfo(name = "Italienisch")
+    private String ritalian;
+
+    @ColumnInfo(name = "Asiatisch")
+    private String rasian;
+
+    @ColumnInfo(name = "Steak")
+    private String rsteak;
+
+    @ColumnInfo(name = "Burger")
+    private String rburger;
+
+    @ColumnInfo(name = "Baguette")
+    private String rbaguette;
+
+    @ColumnInfo(name = "Salat")
+    private String rsalat;
+
+    @ColumnInfo(name = "Fisch")
+    private String rfish;
 
 
 
@@ -36,11 +60,75 @@ public class Restaurants {
         this.rname = rname;
     }
 
-    /*public String getRsalat() {
+    public String getRsalat() {
         return rsalat;
+    }
+
+    public String getRdoener() {
+        return rdoener;
+    }
+
+    public void setRdoener(String rdoener) {
+        this.rdoener = rdoener;
+    }
+
+    public String getRpizza() {
+        return rpizza;
+    }
+
+    public void setRpizza(String rpizza) {
+        this.rpizza = rpizza;
+    }
+
+    public String getRitalian() {
+        return ritalian;
+    }
+
+    public void setRitalian(String ritalian) {
+        this.ritalian = ritalian;
+    }
+
+    public String getRasian() {
+        return rasian;
+    }
+
+    public void setRasian(String rasian) {
+        this.rasian = rasian;
+    }
+
+    public String getRsteak() {
+        return rsteak;
+    }
+
+    public void setRsteak(String rsteak) {
+        this.rsteak = rsteak;
+    }
+
+    public String getRburger() {
+        return rburger;
+    }
+
+    public void setRburger(String rburger) {
+        this.rburger = rburger;
+    }
+
+    public String getRbaguette() {
+        return rbaguette;
+    }
+
+    public void setRbaguette(String rbaguette) {
+        this.rbaguette = rbaguette;
+    }
+
+    public String getRfish() {
+        return rfish;
+    }
+
+    public void setRfish(String rfish) {
+        this.rfish = rfish;
     }
 
     public void setRsalat(String rsalat) {
         this.rsalat = rsalat;
-    }*/
+    }
 }
