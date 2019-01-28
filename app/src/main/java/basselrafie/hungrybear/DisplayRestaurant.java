2f0bbaef460905ddbe726811649ccca4cad39d1a
Bassel Rafie <Bassel.Rafie@gmail.com>
2019-01-28 19:44:00 +0100

    Erste Probe mit success

--- app/src/main/java/basselrafie/hungrybear/DisplayRestaurant.java
+++ app/src/main/java/basselrafie/hungrybear/DisplayRestaurant.java
@@ -12,17 +12,18 @@ public class DisplayRestaurant extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_restaurant);
-        textView = findViewById(R.id.txt_view);
         textView1 = findViewById(R.id.txt_view_2);
+        textView = findViewById(R.id.textView5);
 
         //textView.setText("Hallo");
 
         Bundle extras = getIntent().getExtras();
-        String data = extras.getString("RId");
-        textView.setText(data);
 
         String data1 = extras.getString("RName");
         textView1.setText(data1);
+
+        String Text = extras.getString("RVon");
+        textView.setText(Text + " Uhr");
 
     }
 
