e980b8324b3fdf6183e27fcbecf73f85302a55c4
Despina Tawadros <despinatawadros@hotmail.com>
2019-01-29 23:15:56 +0100

    Dial Action Test

--- app/src/main/java/basselrafie/hungrybear/DisplayRestaurant.java
+++ app/src/main/java/basselrafie/hungrybear/DisplayRestaurant.java
@@ -1,19 +1,38 @@
 package basselrafie.hungrybear;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import static basselrafie.hungrybear.R.string.phone_number;
 
 public class DisplayRestaurant extends AppCompatActivity {
 
+    Button call;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         TextView textView, textView1;
+        Button calleurodoener;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_restaurant);
         textView1 = findViewById(R.id.txt_view_2);
         textView = findViewById(R.id.textView5);
+        calleurodoener = findViewById(R.id.calleurodoener);
+
+
+        calleurodoener.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                callIntent.setData(Uri.parse("tel:123456789"));
+                startActivity(callIntent);
+            }
+        });
 
         //textView.setText("Hallo");
 
