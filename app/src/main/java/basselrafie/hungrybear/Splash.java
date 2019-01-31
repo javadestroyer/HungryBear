6f946ce196d9a4232ba5d2ded2694263c473e71c
VouttaRene <r.voutta@web.de>
2019-01-31 21:59:12 +0100

    Umgecodet für API 23

--- app/src/main/java/basselrafie/hungrybear/Splash.java
+++ app/src/main/java/basselrafie/hungrybear/Splash.java
@@ -1,6 +1,8 @@
 package basselrafie.hungrybear;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,12 +11,12 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class Splash extends AppCompatActivity {
+public class Splash extends Activity {
     private TextView tv;
     private ImageView iv;
 
 
-    @Override
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
@@ -42,5 +44,23 @@ public class Splash extends AppCompatActivity {
             }
         };
         timer.start();
+    }*/
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
